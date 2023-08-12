48cf1ab4bf58e01677735dadccc7f93bcf8dd5ff
Athoy Kanti Ray <114008362+ATHOY43259@users.noreply.github.com>
2023-08-12 15:57:12 +0600

    Add files via upload

---
+++ Quiz_game_with_JSON_Txt_file/main (1).py
@@ -0,0 +1,61 @@
+import json
+
+def load_quiz_data():
+    with open("quiz.json", "r") as file:
+        quiz_data = json.load(file)
+    return quiz_data
+
+def display_question(question_data):
+    print(question_data["question"])
+    for i, option in enumerate(question_data["options"], 1):
+        print(f"{i}. {option}")
+
+def check_answer(user_answer, question_data):
+    correct_answer = question_data["answer"]
+    return user_answer == correct_answer
+
+def play_quiz(quiz_data):
+    score = 0
+    num_questions = len(quiz_data["questions"])
+
+    for question in quiz_data["questions"]:
+        display_question(question)
+        user_answer = input("Your Answer (Enter the option number): ")
+
+        if check_answer(user_answer, question):
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
+
+    final_score = int((score / num_questions) * 100)
+    #print(f"\nQuiz Completed! Your Final Score: {final_score}%")
+    
+    print(f"Final score: {score} / {len(question)}".center(10))
+
+
+def main():
+    print("Quiz Game")
+    print("----------")
+    print("1. Start Quiz")
+    print("2. Exit")
+
+    while True:
+        choice = input("Enter your Choice: ")
+
+        if choice == "1":
+            print("Quiz Loaded!")
+            print("----------")
+            quiz_data = load_quiz_data()
+            play_quiz(quiz_data)
+
+            print("\n1. Retry Quiz")
+            print("2. Exit")
+        elif choice == "2":
+            print("\nExiting...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+# Call the main function directly
+main()
